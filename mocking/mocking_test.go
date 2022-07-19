44878b5b69b86e6693627e76dd789a81f1505a9e
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-07-19 17:13:51 +0500

    order of prints and sleeps tested

--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -2,34 +2,58 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountDown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpyCountDownOperations{}
 
-	CountDown(buffer, spySleeper)
+		CountDown(buffer, spySleeper)
 
-	got := buffer.String()
-	want := `3
-2
-1
-Go!`
+		got := buffer.String()
+		want := "3\n2\n1\nGo!"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 
-	if spySleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
-	}
+	t.Run("sleep in-between the prints", func(t *testing.T) {
+		spySleeperPrinter := &SpyCountDownOperations{}
+		CountDown(spySleeperPrinter, spySleeperPrinter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleeperPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleeperPrinter.Calls)
+		}
+	})
 }
 
-type SpySleeper struct {
-	Calls int
+type SpyCountDownOperations struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *SpyCountDownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
 }
+
+func (s *SpyCountDownOperations) Write(p []byte) (n int, e error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const sleep = "sleep"
+const write = "write"
