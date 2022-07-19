8c335f381b1380a662c3f6c203ab57105286b844
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-07-19 16:29:36 +0500

    #added countdown functionality

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -10,6 +10,13 @@ func main() {
 	CountDown(os.Stdout)
 }
 
+const countDownStart = 3
+const finalWord = "Go!"
+
 func CountDown(writer io.Writer) {
-	fmt.Fprint(writer, "3")
+	for i := countDownStart; i >= 0; i-- {
+		fmt.Fprintln(writer, i)
+	}
+
+	fmt.Fprint(writer, finalWord)
 }
--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -11,7 +11,11 @@ func TestCountDown(t *testing.T) {
 	CountDown(&buffer)
 
 	got := buffer.String()
-	want := "3"
+	want := `3
+2
+1
+0
+Go!`
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
